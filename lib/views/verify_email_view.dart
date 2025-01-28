a936d83c446771929d5588bfb0ee0f744a48cd88
Naggita-Ethel <ethelnaggita@gmail.com>
2025-01-28 17:49:25 +0300

    Initial commit

---
+++ lib/views/verify_email_view.dart
@@ -0,0 +1,44 @@
+import 'package:flutter/material.dart';
+import 'package:mynotes/constants/routes.dart';
+import 'package:mynotes/services/auth/auth_service.dart';
+
+class VerifyEmailView extends StatefulWidget {
+  const VerifyEmailView({super.key});
+
+  @override
+  State<VerifyEmailView> createState() => _VerifyEmailViewState();
+}
+
+class _VerifyEmailViewState extends State<VerifyEmailView> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Verify Email'),
+      ),
+      body: Column(
+        children: [
+          const Text(
+              'We have sent you a verification email, please open it to verify your account'),
+          const Text('If you have not received it, please tap this button:'),
+          TextButton(
+            onPressed: () async {
+              await AuthService.firebase().sendEmailVerification();
+            },
+            child: const Text('Send Email verification'),
+          ),
+          TextButton(
+            onPressed: () async {
+              await AuthService.firebase().logOut();
+              Navigator.of(context).pushNamedAndRemoveUntil(
+                registerRoute,
+                (route) => false,
+              );
+            },
+            child: const Text('Restart'),
+          )
+        ],
+      ),
+    );
+  }
+}
